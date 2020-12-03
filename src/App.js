a3ab8871e2c1bde08a2f13c4bd9f708d9e1d72fb
Cisse23 <baker.cisse@gmail.com>
2020-12-03 13:07:49 +0200

    Folder tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hello!</h1>
+      <Folder name='my_desktop' size='12Mb'>
+        <Folder name='music' size='2.3Gb'>
+          <File name='paint_it_black.mp4' />
+          <File name='the_river.mp4' />
+          <File name='hallowed_be_thy_name.mp4' />
+        </Folder>
+        <File name='recipe.doc' />
+        <File name='mega_excel.xls' />
+      </Folder>
+      <Folder name='pictures' size='6Gb'>
+        <File name='cats1.jpg' />
+        <File name='cats23.tiff' />
+        <File name='cats44.jpg' />
+        <File name='cats56.jpg' />
+        <File name='cats71.jpg' />
+      </Folder>
+
+    </div >
   );
 }
+
+const Folder = (props) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { name, children } = props; //destructure name, isopen and chldren from props(so i don't need to write props all the time)
+  const direction = isOpen ? 'down' : 'right'
+
+  const handleClick = () => setIsOpen(!isOpen)
+
+
+  return (
+    <div >
+      <span onClick={handleClick}>
+        <i className="blue folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>  {/* use backtick interpolate strings in JS*/}
+      </span>
+      { name}
+      <div style={{ marginLeft: '17px' }}>
+        {isOpen ? children : null}
+      </div>
+
+    </div >
+  )
+
+};
+
+const File = (props) => {
+  const { name } = props;
+  const fileExtension = name.split('.')[1];
+  const fileIcons = {
+    mp4: 'headphones',
+    jpg: 'file image outline',
+    tiff: 'file image'
+
+  }
+
+  return (
+    <div>
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
+    </div>
+  )
+};
 
 export default App;
